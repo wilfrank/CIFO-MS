f48a43df51e15762ca1d56ca7c3133a489694b2b
wilfrank.montalvo <wmontalvop@eafit.com>
2023-09-30 16:14:34 -0500

    Updating files configuration

--- CiFo-Sln/Auth.API/Program.cs
+++ CiFo-Sln/Auth.API/Program.cs
@@ -36,11 +36,13 @@ namespace Auth.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
